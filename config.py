5418fb4e42a99f3ebf6a89686c9f1f84786684dc
Asilbek <asilbek.sadullayev000@example.com>
2025-08-04 20:56:10 +0500

    nima

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
 # config.py
-BOT_TOKEN = "7646223205:AAHBuit0gTN702jLKHo57_ZA4ypuURFuh1A"  # @BotFather dan olingan token
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("TOKEN")  # @BotFather dan olingan token
 DEFAULT_CHANNEL_ID = ""  # Kanal ID si, masalan: @YourChannel
